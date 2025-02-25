d32222645847007e9114d30df50d0705c3de1151
eagle777-krs <williamgarfield67@gmail.com>
2025-02-25 16:49:10 +0300

    add format as arg

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -7,4 +7,6 @@ def gendiff():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
+    parser.add_argument('-f', '--format', help = 'set format of output')
+
     return parser.parse_args()
